202e9a3eeed10381f9b9f1354978be3a6d948004
Maxime Girard <mx.girard42@gmail.com>
2019-03-05 14:04:11 +0100

    introduce a variable in a basic html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-const App = ()=> {
+/*const App = ()=> {
   return (
     <div>
       <h1 className="display-1">Weather App</h1>
     </div>
   );
-};
+};*/
+
+export const appTitle = "Awesome Weather App";
+
+function App(){
+    return (
+        <div className="container">
+            <div className="display-2">{appTitle}</div>
+        </div>
+    );
+}
 
 export default App;
